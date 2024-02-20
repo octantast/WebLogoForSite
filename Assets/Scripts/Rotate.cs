b3df49554551c444c79a2145e695b45ae5d1d6c0
Dariia Tsalikova <134861093+octantast@users.noreply.github.com>
2024-02-21 00:17:46 +0200

    Init

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+   public Transform objectToRotate; // Ссылка на объект, который нужно вращать
+    public float rotationSpeed = 5f; // Скорость вращения
+    public float damping = 0.5f; // Затухание вращения
+
+    private Vector3 lastMousePosition; // Последняя позиция мыши
+    private Vector3 rotationDirection;
+
+    void Update()
+    {
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    Vector3 currentMousePosition = Input.mousePosition;
+                    Vector3 difference = currentMousePosition - lastMousePosition;
+                    rotationDirection = new Vector3(-difference.y, difference.x, 0f);
+                    objectToRotate.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);
+                    lastMousePosition = currentMousePosition;
+                }
+                else
+                {
+                    objectToRotate.Rotate(rotationDirection * rotationSpeed * damping * Time.deltaTime);
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 currentMousePosition = Input.mousePosition;
+                Vector3 difference = currentMousePosition - lastMousePosition;
+                rotationDirection = new Vector3(-difference.y, difference.x, 0f);
+                objectToRotate.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);
+                lastMousePosition = currentMousePosition;
+            }
+            else
+            {
+                objectToRotate.Rotate(rotationDirection * rotationSpeed * damping * Time.deltaTime);
+            }
+        }
+    }
+
+    void Start()
+    {
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            lastMousePosition = Input.GetTouch(0).position;
+        }
+        else
+        {
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+}
